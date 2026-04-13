32337c239e1e3b1001d17c3286fa2e95d774f4a5
qerib-seferli <seferovqerib09@gmail.com>
2026-04-13 20:23:14 +0400

    Update Supabase configuration with new URL and key

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -3,6 +3,6 @@
 // Bu faylda öz SUPABASE_URL və SUPABASE_ANON_KEY dəyərlərinizi əlavə edin
 // ================================
 window.SUPABASE_CONFIG = {
-  url: "SUPABASE_URL_BURAYA_YAZ",
-  anonKey: "SUPABASE_ANON_KEY_BURAYA_YAZ"
+  url: "https://vytmnkywcpeyahgbvjdi.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dG1ua3l3Y3BleWFoZ2J2amRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5Mzg2NTQsImV4cCI6MjA5MTUxNDY1NH0.gBqTbzaCwXKQr132Gfzp8SGjpOr9cmiYwGl8Srw221Y"
 };
